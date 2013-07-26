b759088fb337b9d96a820b022bea48410f82592a
Jack Schultz <jackschultz23@gmail.com>
2013-07-26 21:05:41 +0000

    some requirements and settings changes

--- streakflow/settings.py
+++ streakflow/settings.py
@@ -83,7 +83,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/jack/general/static/'
+STATIC_ROOT = '/home/jackschultz/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
