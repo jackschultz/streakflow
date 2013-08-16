b25c62d04caeda0ce118a0bfb1d71e1414f85d8c
Jack Schultz <jackschultz23@gmail.com>
2013-08-15 20:20:17 -0400

    create dummy goal on member create

--- streakflow/apps/members/signals.py
+++ streakflow/apps/members/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from registration.signals import user_registered, user_activated
 from django.db.models.signals import post_save
 from models import Member
+from streakflow.apps.goals.models import Goal
 
 @receiver(user_registered)
 def create_callback(sender, *args, **kwargs):
@@ -10,6 +11,11 @@ def create_callback(sender, *args, **kwargs):
   member = Member()
   member.user = user
   member.save()
+  #now we want to create a goal for this member,
+  #so they have something and get goals
+  goal = Goal(goal_name="Create more goals!",num_per_frame=1,time_frame_len='d')
+  goal.member = member
+  goal.save()
 
 @receiver(post_save,sender=User)
 def create_profile(sender, **kwargs):
